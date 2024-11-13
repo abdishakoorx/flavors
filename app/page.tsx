e612da9a6367b41021be565d234a3984ac69056c
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-11-13 23:57:41 +0300

    + small

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,25 @@ import Nav from "./_components/Nav";
 import FAQSection from "./_components/FAQSection";
 import OfferingSection from "./_components/OfferingSection";
 import HowItWorks from "./_components/HowItWorks";
+import Pricing from "./_components/Pricing";
+import { Testimonial } from "./_components/Testimonial";
+import CTASection from "./_components/CTASection";
+import Footer from "./_components/Footer";
 
 export default function Home() {
   return (
-    <div className="md:px-8 lg:px-12 space-y-20">
-      <Nav />
-      <Hero/>
-      <OfferingSection/>
-      <HowItWorks/>
-      <FAQSection/>
+    <div className="space-y-20">
+      <main className="md:px-8 lg:px-12 space-y-20">
+        <Nav />
+        <Hero />
+        <OfferingSection />
+        <HowItWorks />
+        <Pricing />
+        <FAQSection />
+        <Testimonial />
+        <CTASection />
+      </main>
+      <Footer />
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,17 +59,60 @@ const config: Config = {
   				DEFAULT: 'hsl(var(--sidebar-background))',
   				foreground: 'hsl(var(--sidebar-foreground))',
   				primary: 'hsl(var(--sidebar-primary))',
-				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-				accent: 'hsl(var(--sidebar-accent))',
-				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-				border: 'hsl(var(--sidebar-border))',
-				ring: 'hsl(var(--sidebar-ring))'
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			marquee: {
+  				from: {
+  					transform: 'translateX(0)'
+  				},
+  				to: {
+  					transform: 'translateX(calc(-100% - var(--gap)))'
+  				}
+  			},
+  			'marquee-vertical': {
+  				from: {
+  					transform: 'translateY(0)'
+  				},
+  				to: {
+  					transform: 'translateY(calc(-100% - var(--gap)))'
+  				}
+  			},
+  			'shimmer-slide': {
+  				to: {
+  					transform: 'translate(calc(100cqw - 100%), 0)'
+  				}
+  			},
+  			'spin-around': {
+  				'0%': {
+  					transform: 'translateZ(0) rotate(0)'
+  				},
+  				'15%, 35%': {
+  					transform: 'translateZ(0) rotate(90deg)'
+  				},
+  				'65%, 85%': {
+  					transform: 'translateZ(0) rotate(270deg)'
+  				},
+  				'100%': {
+  					transform: 'translateZ(0) rotate(360deg)'
+  				}
+  			}
+  		},
+  		animation: {
+  			marquee: 'marquee var(--duration) infinite linear',
+  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
+  			'shimmer-slide': 'shimmer-slide var(--speed) ease-in-out infinite alternate',
+  			'spin-around': 'spin-around calc(var(--speed) * 2) infinite linear'
   		}
   	}
   },
